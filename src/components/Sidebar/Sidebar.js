96de69e4dfa8d53eed059727c6168b2bc35d1c59
Colton Travers <contactcolt@gmail.com>
2018-03-10 21:35:52 -0600

    Remove components, cleanup sidebar css

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -11,9 +11,21 @@ class Sidebar extends Component {
     const { activeItem } = this.state;
     return (
       <Menu pointing secondary vertical>
-        <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
-        <Menu.Item name='messages' active={activeItem === 'messages'} onClick={this.handleItemClick} />
-        <Menu.Item name='friends' active={activeItem === 'friends'} onClick={this.handleItemClick} />
+        <Menu.Item
+          name="home"
+          active={activeItem === 'home'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name="messages"
+          active={activeItem === 'messages'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name="friends"
+          active={activeItem === 'friends'}
+          onClick={this.handleItemClick}
+        />
       </Menu>
     );
   }
